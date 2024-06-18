36c566e052e07832e08527ec9036bef5e1160395
dylanCai9 <22110240060@m.fudan.edu.cn>
2024-06-18 16:21:38 +0800

    extend mmcs

--- primitives/src/mmcs/taptree.rs
+++ primitives/src/mmcs/taptree.rs
@@ -128,7 +128,6 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
             .peekable();
         let max_height = leaves_largest_first.peek().unwrap().height();
         let log_max_height = log2_ceil_usize(max_height);
-
         //println!("max height:{:?}", max_height);
         let mut tree_builder = TreeBuilder::<NUM_POLY>::new();
 
@@ -161,8 +160,12 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
 
         for index in 0..max_height {
             if leaf_ys[index].len() != 0 {
-                //println!("index:{:?}, ys:{:?}", index, leaf_ys[index]);
-                let leaf = PointsLeaf::new(index, &leaf_xs[index], &leaf_ys[index]);
+                println!("index:{:?}, ys:{:?}", index, leaf_ys[index]);
+                let leaf = PointsLeaf::new(
+         index,
+                    &leaf_xs[index],
+                    &leaf_ys[index],
+                );
                 self.add_leaf(&mut tree_builder, &leaf);
             }
         }
@@ -249,6 +252,7 @@ impl<const NUM_POLY: usize> TreeBuilder<NUM_POLY> {
             let mut todo: Vec<NodeInfo> = Vec::new();
             let mut a_start_idx = 0usize; // will be updated after finishing combining two nodes.
 
+            
             for (a, b) in node_tuples {
                 let a_leaf_size = a.leaf_nodes().len();
                 let a_end_idx = a_start_idx + a_leaf_size;
@@ -277,7 +281,6 @@ impl<const NUM_POLY: usize> TreeBuilder<NUM_POLY> {
                 a_start_idx += a_leaf_size + b_leaf_size;
             }
             working_nodes = todo;
-
             todo = Vec::new();
         }
         BasicTree {
